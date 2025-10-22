59328a35a431aa808ce698a8b942fbcc3fa7632f
ArneCrash <arne31.claerhout@gmail.com>
2025-10-22 11:24:09 +0200

    Added new files to naiveSrc

--- Code/naiveSrc/Main.java
+++ Code/naiveSrc/Main.java
@@ -2,6 +2,10 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -10,39 +14,71 @@ public class Main {
      **********************************/
 
     public static void main(String[] args) throws IOException {
-        ArgumentParser argsParser = new ArgumentParser(args);
+
+        // We get the arguments from our bash script, we know the order:
+        // First: the coloring
+        Coloring coloring = Coloring.getColoring(args[0]);
+        // Second: filter
+        //TODO
+
+        // Third: overview
+        boolean overview = false;
+        TreeMap<Integer, Integer> cNumbers = new TreeMap<>();
+        if (args[1].equals("true")) {
+            // We know there is only an extra argument if the output should be raw
+            overview = true;
+        }
+
+        // Last: whether the output should be raw
+        boolean raw = false;
+        if (args[2].equals("true")) {
+            // We know there is only an extra argument if the output should be raw
+            raw = true;
+        }
+
+
+        if (!raw) System.out.println("Received coloring: " + coloring);
+
+
+        // We start reading the graphs
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         String line;
 
-        String inputColoring = argsParser.getValue("-c");
+        long start = System.currentTimeMillis();
 
-        Coloring coloring = Coloring.getColoring(inputColoring);
-        System.out.println("Received coloring: " + coloring);
-
-
-        if (argsParser.getValue("-m") == null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-            // As long as there is something to read from stdin, we read it.
-            while ((line = reader.readLine()) != null) {
-
-                Graph graph = new Graph(line);
-                System.out.print(line + ": ");
-                System.out.println(graph.findChromaticNumberNaive(coloring));
-
-            }
-            System.out.println("All graphs have been processed.");
-
-
-        } else {
-            // Manual is requested
-            line = argsParser.getValue("-m");
+        // As long as there is something to read from stdin, we read it.
+        while ((line = reader.readLine()) != null) {
 
             Graph graph = new Graph(line);
-            System.out.print(line + ": ");
-            System.out.println(graph.findChromaticNumberNaive(coloring));
+
+            if (overview) {
+                int c = graph.findChromaticNumberNaive(coloring);
+                cNumbers.merge(c, 1, Integer::sum);
+            } else {
+                if (!raw) System.out.print(line + ": ");
+                System.out.println(graph.findChromaticNumberNaive(coloring));
+            }
+
 
         }
 
+        long end = System.currentTimeMillis();
+
+        String finalTime = String.format("%.2f", (end - start)/1000.0); // in seconds
+
+        if (!raw) {
+            System.out.println("All graphs have been processed.");
+            System.out.println("Final time: " + finalTime);
+        }
+
+        if (overview) {
+            for (Integer c : cNumbers.keySet()) {
+                System.out.println(cNumbers.get(c) + " graphs : chrom=" + c);
+                // This is in sorted order as it was stored in a TreeMap
+            }
+            System.out.println(cNumbers.values().stream().mapToInt(x -> x).sum() + " graphs altogether; cpu=" + finalTime + " sec");
+        }
 
 
     }
---
+++ Code/naiveSrc/colorScript.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Function that generates the plantri output in a range of vertices
+gen_range_graphs() {
+  if [[ "$raw" == false ]]; then
+    # Only if we are not in raw mode do we print this.
+    echo "Generating graphs from $1 to $2 vertices." >&2 # We print to stderr, so this isn't on stdout
+  fi
+  for num in $(seq "$1" "$2"); do
+    ./../../plantri55/plantri -g "$num" 2>/dev/null # We get rid of the extra printing to the terminal
+  done
+}
+
+read_stdin() {
+  while IFS= read -r line; do
+      echo "$line"
+    done
+}
+
+
+# Default values for our flags
+coloring=""
+filter=""
+manual=""
+raw=false
+overview=false
+
+if [[ "$1" != -* ]]; then
+  # Number of vertices is given
+
+  # We extract them
+  if [[ "$1" == *:* ]]; then
+    # Split the argument into start and end
+    IFS=':' read -r startn endn <<< "$1"
+  else
+    # Only one value given; use it as both start and end
+    startn="$1"
+    endn="$1"
+  fi
+  shift 1
+
+fi
+
+
+# Parse options
+POSITIONAL_ARGS=()
+
+# Loop over all flags and their values
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -c|--coloring)
+      coloring="$2"
+      shift 2
+      ;;
+    -m|--manual)
+      manual="$2"
+      shift 2
+      ;;
+    -f|--filter)
+      filter="$2"
+      shift 2
+      ;;
+    --overview)
+      overview=true
+      shift
+      ;;
+    --raw)
+      raw=true
+      shift # We only shift once as there is no value associated with raw
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+# We make sure to recompile the code
+javac Main.java
+if [[ "$raw" == false ]]; then
+  # Only if we are not in raw mode do we print this.
+  echo "Code compiled."
+fi
+
+
+#echo "Coloring: $coloring"
+#echo "Filename: $filename"
+#echo "Manual: $manual"
+
+if [[ -z "$filter" ]]; then
+  # No filter given
+  if [[ -z "$startn" && -z "$manual" ]]; then
+    # There is no n given and we don't want to manually give a graph -> ERROR
+    # We only check startn, as both are either filled or not filled
+    echo "Error: No filter or number of vertices given."
+    exit 1
+  fi
+else
+  # Filter given, we use this over number of vertices
+  # PLACEHOLDER
+  n=$((filter + 0)) # Set n to the filter
+  # PLACEHOLDER
+fi
+
+if [[ -z "$manual" ]]; then
+  # Manual not set
+  gen_range_graphs "$startn" "$endn" "$raw" | java Main "$coloring" "$overview" "$raw"
+elif [[ "$manual" == "stream" ]]; then
+  read_stdin | java Main "$coloring" "$overview" "$raw"
+else
+  echo "$manual" | java Main "$coloring" "$overview" "$raw"
+fi
+
+
+
