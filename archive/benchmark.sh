aa86b8f0302f9ce42b27d3b6ac5d99f66a32e1ba
ArneClaerhout <arne31.claerhout@gmail.com>
2026-04-23 21:23:19 +0200

    Update benchmark

--- archive/benchmark.sh
+++ archive/benchmark.sh
@@ -6,33 +6,42 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$script_dir" || exit 1
 
 echo "Starting benchmark."
-path="$1"
-shift 1
+n="$1"
+path="$2"
+shift 2
+
+LOOPS=3
+
+printf "%-15s | %-15s\n" "Configuration" "Time (ms)"
+echo "--------------------------------------------------"
+
+configs=("proper" "odd" "iUMo" "iUMc" "pUMo" "pUMc" "iCFo" "iCFc" "pCFo")
 
 TIMEFORMAT='%R'
 
 start_time=$(date +%s%N)
 
-printf "Proper: "
-time "./$path" 14 -c proper -o "$@" >/dev/null 2>&1
-printf "Odd: "
-time "./$path" 14 -c odd -o "$@" >/dev/null 2>&1
-printf "iUMo: "
-time "./$path" 14 -c iUMo -o "$@" >/dev/null 2>&1
-printf "iUMc: "
-time "./$path" 14 -c iUMc -o "$@" >/dev/null 2>&1
-printf "pUMo: "
-time "./$path" 14 -c pUMo -o "$@" >/dev/null 2>&1
-printf "pUMc: "
-time "./$path" 14 -c pUMc -o "$@" >/dev/null 2>&1
-printf "iCFo: "
-time "./$path" 14 -c iCFo -o "$@" >/dev/null 2>&1
-printf "iCFc: "
-time "./$path" 14 -c iCFc -o "$@" >/dev/null 2>&1
-printf "pCFo: "
-time "./$path" 14 -c pCFo -o "$@" >/dev/null 2>&1
+
+
+for config in "${configs[@]}"; do
+    TOTAL_TIME=0
+    for ((i=1; i<=LOOPS; i++)); do
+        START=$(date +%s%N)
+        "./$path" "$n" -c "$config" -o "$@" >/dev/null 2>&1
+        END=$(date +%s%N)
+        
+        DIFF=$(( (END - START) / 1000000 ))
+        TOTAL_TIME=$((TOTAL_TIME_NAUTY + DIFF))
+    done
+    AVG_TIME=$(echo "scale=3; $TOTAL_TIME / $LOOPS" | bc)
+    printf "%-15s | %-15s\n" "$config" "$AVG_TIME"
+done
+
+
+
+echo "--------------------------------------------------"
 
 end_time=$(date +%s%N)
 
-duration=$(((end_time - start_time) / 1000000))
-echo "Execution time: $duration ms"
+duration=$(((end_time - start_time) / (1000000 * LOOPS)))
+echo "Total execution time: $duration ms"
